13d76c0dd0592db452ace5327e58b5a674661ca6
DavidPv07 <rusdavid565@gmail.com>
2024-10-11 12:14:40 +0200

    Add some fwb stubs from Oplus

---
+++ core/java/com/oplus/theme/OplusThemeUtil.java
@@ -0,0 +1,7 @@
+package com.oplus.theme;
+import android.compat.annotation.UnsupportedAppUsage;
+public class OplusThemeUtil {
+    /** @hide */
+    @UnsupportedAppUsage
+    public static String CUSTOM_THEME_PATH = "/data/theme/com.oplus.camera";
+}
---
+++ core/java/com/oplus/util/OplusTypeCastingHelper.java
@@ -0,0 +1,2 @@
+package com.oplus.util;
+public final class OplusTypeCastingHelper { }
